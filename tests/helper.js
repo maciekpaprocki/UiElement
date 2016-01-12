d0467e94d4fec4c47e76de64ab9f913f2639c3df
Maciej Paprocki <maciekpaprocki@gmail.com>
2016-01-12 09:39:53 +0000

    fighting with karma

---
+++ tests/helper.js
@@ -0,0 +1,7 @@
+var helper = require('../uielement');
+
+describe("A suite", function() {
+    it("contains spec with an expectation", function() {
+        expect(true).toBe(true);
+    });
+});
--- uiElement.js
+++ uiElement.js
@@ -1,113 +1,136 @@
-/**
- * Created by maciejpaprocki on 26/12/2015.
- */
 
-/**
- * Help classes used across the project
- * @type {{map: helper.map, forEach: helper.forEach}}
- */
-var helper = {
-    /**
-     *
-     * @param nodeList
-     * @param callable
-     * @param thisArg
-     * @return array
-     */
-    map: function (nodeList, callable, thisArg) {
-        if (!thisArg) {
-            thisArg = {};
-        }
-        var res = [];
-        for (var i = 0, l = nodeList.length; i < l; i++) {
-            res.push(callable.call(thisArg, nodeList[i], i, nodeList));
-        }
-        return res;
-    },
-    /**
-     *
-     * @param nodeList
-     * @param callable
-     * @param thisArg
-     * @return array
-     */
-    forEach: function (nodeList, callable, thisArg) {
-        helper.map.apply(null,arguments);
-        return nodeList;
-    }
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
 
-}
-/**
- * Global iterator used for assigning id's to use functions find;
- * @type {number}
- */
-globalSelectorIterator = 0;
-/**
- *
- * @node scope NodeElement
- * @constructor
- */
-var Selector = function (scope) {
-    this.scope = scope;
+        define(["uielement"], function (uielement) {
+            return (root.myModule = factory(uielement));
+        });
+    } else if (typeof module === "object" && module.exports) {
 
-    if (scope.id) {
-        this.id = scope.id;
+        module.exports = (root.uielement = factory(require("uielement")));
     } else {
-        this.id = 'uniqueUiElementId_' + globalSelectorIterator++;
-        scope.id = this.id;
+        root.uielement = factory(root.uielement);
     }
-};
-/**
- * Function responsible for finding nodes by string in scope of Node
- * @param selector
- * @returns {NodeList}
- */
-Selector.prototype.find = function (selector) {
-    return document.querySelectorAll(this.id + ' ' + selector);
-};
-/**
- * Function responsible for kid nodes of node
- * @param selector
- * @returns {NodeList}
- */
-Selector.prototype.children = function () {
-    return document.querySelectorAll(this.id + ' > ' + selector);
-};
-/**
- * Extendable Class for uiElement
- * @param selector String
- * @param options
- * @constructor
- */
-var UiElement = function (el, options) {
-    this.el = el;
-    this.$el = new Selector(el);
-    this.options = options;
-};
+}(this, function (uielement) {
 
-UiElement.prototype.bind = function () {
+    /**
+     * Created by maciejpaprocki on 26/12/2015.
+     */
 
-};
-/**
- * Static classes for UiELement
- */
-/**
- * The most important class for extending uiElement.
- * All the SubClasses will use this function
- * @param object object object to be extended
- */
-UiElement.extend = function (object) {
+    /**
+     * Help classes used across the project
+     * @type {{map: helper.map, forEach: helper.forEach}}
+     */
+    var helper = {
+        /**
+         *
+         * @param nodeList
+         * @param callable
+         * @param thisArg
+         * @return array
+         */
+        map: function (nodeList, callable, thisArg) {
+            if (!thisArg) {
+                thisArg = {};
+            }
+            var res = [];
+            for (var i = 0, l = nodeList.length; i < l; i++) {
+                res.push(callable.call(thisArg, nodeList[i], i, nodeList));
+            }
+            return res;
+        },
+        /**
+         *
+         * @param nodeList
+         * @param callable
+         * @param thisArg
+         * @return array
+         */
+        forEach: function (nodeList, callable, thisArg) {
+            helper.map.apply(null, arguments);
+            return nodeList;
+        }
 
-};
-UiElement.attach = function (selector) {
+    }
+    /**
+     * Global iterator used for assigning id's to use functions find;
+     * @type {number}
+     */
+    globalSelectorIterator = 0;
+    /**
+     *
+     * @node scope NodeElement
+     * @constructor
+     */
+    var Selector = function (scope) {
+        this.scope = scope;
 
-    var objectsArray = [];
-    var els = document.querySelectorAll(selector);
-    objectsArray = helper.map(els,function(){
+        if (scope.id) {
+            this.id = scope.id;
+        } else {
+            this.id = 'uniqueUiElementId_' + globalSelectorIterator++;
+            scope.id = this.id;
+        }
+    };
+    /**
+     * Function responsible for finding nodes by string in scope of Node
+     * @param selector
+     * @returns {NodeList}
+     */
+    Selector.prototype.find = function (selector) {
+        return document.querySelectorAll(this.id + ' ' + selector);
+    };
+    /**
+     * Function responsible for kid nodes of node
+     * @param selector
+     * @returns {NodeList}
+     */
+    Selector.prototype.children = function () {
+        return document.querySelectorAll(this.id + ' > ' + selector);
+    };
+    /**
+     * Extendable Class for uiElement
+     * @param selector String
+     * @param options
+     * @constructor
+     */
+    var UiElement = function (el, options) {
+        this.el = el;
+        this.$el = new Selector(el);
+        this.options = options;
+    };
 
-    })
-};
-module.exports.Selector = Selector;
-module.exports.UiElement = UiElement;
-module.exports.helper = helper;
+    UiElement.prototype.bind = function () {
 
+    };
+    /**
+     * Static classes for UiELement
+     */
+    /**
+     * The most important class for extending uiElement.
+     * All the SubClasses will use this function
+     * @param object object object to be extended
+     */
+    function extend(object) {
+        var propertyName;
+        for(propertyName in object){
+            if(object.hasOwnProperty(propertyName)){
+
+            }
+        }
+    };
+    UiElement.attach = function (selector) {
+
+        var objectsArray = [];
+        var els = document.querySelectorAll(selector);
+        objectsArray = helper.map(els, function () {
+
+        })
+    };
+    return {
+        Selector: Selector,
+        UiElement: UiElement,
+        helper: helper
+    }
+
+}));
